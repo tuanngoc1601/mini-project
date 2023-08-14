cb1f4bec6410bca9e098e33b6b465c9cb88b4f55
tuanngoc1601 <tuanngoc12340@gmail.com>
2023-08-14 16:51:32 +0700

    add name user and logout account

--- productDetail.php
+++ productDetail.php
@@ -21,72 +21,70 @@ if (!isset($data['error'])) {
 
 ?>
 
+<?php
+if (isset($_GET['action']) && $_GET['action'] == 'logout') {
+    Session::destroy();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title>Product detail page</title>
-        <link rel="stylesheet" href="./assets/stylesheets/productStyle.css" />
-        <link
-            rel="stylesheet"
-            href="./assets/stylesheets/productDetailStyle.css"
-        />
-        <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-            integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-            crossorigin="anonymous"
-            referrerpolicy="no-referrer"
-        />
-    </head>
 
-    <body>
-        <div class="container">
-            <div class="sidebar">
-                <div class="image-text logo">
-                    <div class="image">
-                        <img src="./assets/images/logo.png" alt="logo" />
-                    </div>
-                    <div class="text">
-                        <p>Product management service</p>
-                    </div>
+<head>
+    <meta charset="utf-8" />
+    <title>Product detail page</title>
+    <link rel="stylesheet" href="./assets/stylesheets/productStyle.css" />
+    <link rel="stylesheet" href="./assets/stylesheets/productDetailStyle.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="sidebar">
+            <div class="image-text logo">
+                <div class="image">
+                    <img src="./assets/images/logo.png" alt="logo" />
                 </div>
-                <div class="image-text">
-                    <div class="image">
-                        <i class="fa-solid fa-cart-shopping"></i>
-                    </div>
-                    <a href="/productList.php">
-                        <div class="text">
-                            <p>Product list</p>
-                        </div>
-                    </a>
+                <div class="text">
+                    <p>Product management service</p>
                 </div>
             </div>
-            <div class="main-wrapper">
-                <div class="navbar">
-                    <div class="icon">
-                        <img
-                            src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
-                            alt="user"
-                        />
-                        <p>Test user</p>
-                    </div>
-                    <div class="logout">
-                        <i class="fa-solid fa-right-from-bracket"></i>
-                    </div>
+            <div class="image-text">
+                <div class="image">
+                    <i class="fa-solid fa-cart-shopping"></i>
                 </div>
-                <div class="content">
-                    <div class="title">
-                        <span>></span>
-                        <h1>Product detail page</h1>
+                <a href="/mini-project/productList.php">
+                    <div class="text">
+                        <p>Product list</p>
                     </div>
-                    <div class="detail">
-                        <?php
-                            if ($product != NULL) {
-                                $name = $product['name'];
-                                $price = $product['price'];
-                                $description = $product['description'];
-                                echo "<div class='image'>
+                </a>
+            </div>
+        </div>
+        <div class="main-wrapper">
+            <div class="navbar">
+                <div class="icon">
+                    <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="user" />
+                    <?php
+                        $name = Session::get("name");
+                        echo "<p>$name</p>";
+                    ?>
+                </div>
+                <div class="logout">
+                    <a href="?action=logout"><i class="fa-solid fa-right-from-bracket"></i></a>
+                </div>
+            </div>
+            <div class="content">
+                <div class="title">
+                    <span>></span>
+                    <h1>Product detail page</h1>
+                </div>
+                <div class="detail">
+                    <?php
+                    if ($product != NULL) {
+                        $name = $product['name'];
+                        $price = $product['price'];
+                        $description = $product['description'];
+                        echo "<div class='image'>
                                     <img
                                         src='https://media.istockphoto.com/id/533728266/photo/thin-golden-frame-with-clipping-path.jpg?s=612x612&w=0&k=20&c=07L_oV-EyMif0W_iEsEYy8TbOFXsIV7Mm6Vcyp-RNbw='
                                         alt='product image'
@@ -107,16 +105,17 @@ if (!isset($data['error'])) {
                                         </div>
                                         <div class='action'>
                                             <button class='btn primary'>Edit</button>
-                                            <a href='/productList.php'><button class='btn secondary'>Cancel</button></a>
+                                            <a href='/mini-project/productList.php'><button class='btn secondary'>Cancel</button></a>
                                         </div>
                                 </div>";
-                            } else {
-                                echo "<h2>No product found</h2>";
-                            }
-                        ?>
-                    </div>
+                    } else {
+                        echo "<h2>No product found</h2>";
+                    }
+                    ?>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
--- productList.php
+++ productList.php
@@ -23,132 +23,117 @@ if (!isset($data['error'])) {
 
 ?>
 
+<?php
+if (isset($_GET['action']) && $_GET['action'] == 'logout') {
+    Session::destroy();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title>Product list page</title>
-        <link rel="stylesheet" href="./assets/stylesheets/productStyle.css" />
-        <link
-            rel="stylesheet"
-            href="./assets/stylesheets/productListStyle.css"
-        />
-        <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-            integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-            crossorigin="anonymous"
-            referrerpolicy="no-referrer"
-        />
-    </head>
 
-    <body>
-        <div class="container">
-            <div class="sidebar">
-                <div class="image-text logo">
-                    <div class="image">
-                        <img src="./assets/images/logo.png" alt="logo" />
-                    </div>
-                    <div class="text">
-                        <p>Product management service</p>
-                    </div>
+<head>
+    <meta charset="utf-8" />
+    <title>Product list page</title>
+    <link rel="stylesheet" href="./assets/stylesheets/productStyle.css" />
+    <link rel="stylesheet" href="./assets/stylesheets/productListStyle.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="sidebar">
+            <div class="image-text logo">
+                <div class="image">
+                    <img src="./assets/images/logo.png" alt="logo" />
                 </div>
-                <div class="image-text">
-                    <div class="image">
-                        <i class="fa-solid fa-cart-shopping"></i>
-                    </div>
-                    <a href="/productList.php">
-                        <div class="text">
-                            <p>Product list</p>
-                        </div>
-                    </a>
+                <div class="text">
+                    <p>Product management service</p>
                 </div>
             </div>
-            <div class="main-wrapper">
-                <div class="navbar">
-                    <div class="icon">
-                        <img
-                            src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
-                            alt="user"
-                        />
-                        <p>Test user</p>
-                    </div>
-                    <div class="logout">
-                        <i class="fa-solid fa-right-from-bracket"></i>
-                    </div>
+            <div class="image-text">
+                <div class="image">
+                    <i class="fa-solid fa-cart-shopping"></i>
                 </div>
-                <div class="content">
-                    <div class="title">
-                        <span>></span>
-                        <h1>Product list page</h1>
+                <a href="/mini-project/productList.php">
+                    <div class="text">
+                        <p>Product list</p>
                     </div>
-                    <div class="new">
-                        <a href="/newProduct.php">
-                            <button class="btn success">
-                                <i class="fa-solid fa-circle-plus"></i>
-                                <span>New</span>
-                            </button>
-                        </a>
-                    </div>
-                    <div class="product-table">
-                        <table border="1">
-                            <tr>
-                                <th style="width: 5%">ID</th>
-                                <th style="width: 50%">Name</th>
-                                <th style="width: 15%">Price</th>
-                                <th style="width: 30%">Action</th>
-                            </tr>
-                            <?php
-                            if (!empty($products)) {
-                                foreach ($products as $product) {
-                                    $id = $product[0];
-                                    $name = $product[1];
-                                    $price = $product[2];
-                                    echo "<tr>
+                </a>
+            </div>
+        </div>
+        <div class="main-wrapper">
+            <div class="navbar">
+                <div class="icon">
+                    <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="user" />
+                    <?php
+                        $name = Session::get("name");
+                        echo "<p>$name</p>";
+                    ?>
+                </div>
+                <div class="logout">
+                    <a href="?action=logout"><i class="fa-solid fa-right-from-bracket"></i></a>
+                </div>
+            </div>
+            <div class="content">
+                <div class="title">
+                    <span>></span>
+                    <h1>Product list page</h1>
+                </div>
+                <div class="new">
+                    <a href="/mini-project/newProduct.php" style="display: inline-block;">
+                        <button class="btn success">
+                            <i class="fa-solid fa-circle-plus"></i>
+                            <span>New</span>
+                        </button>
+                    </a>
+                </div>
+                <div class="product-table">
+                    <table border="1">
+                        <tr>
+                            <th style="width: 5%">ID</th>
+                            <th style="width: 50%">Name</th>
+                            <th style="width: 15%">Price</th>
+                            <th style="width: 30%">Action</th>
+                        </tr>
+                        <?php
+                        if (!empty($products)) {
+                            foreach ($products as $product) {
+                                $id = $product[0];
+                                $name = $product[1];
+                                $price = $product[2];
+                                echo "<tr>
                                     <td> $id </td>
                                     <td> $name </td>
                                     <td> $price </td>
                                     <td class='action'>
-                                        <a href='/productDetail.php?id=$id'><button class='btn info text-small'>View</button></a>
+                                        <a href='/mini-project/productDetail.php?id=$id'><button class='btn info text-small'>View</button></a>
                                         <button class='btn primary text-small'>Edit</button>
                                         <button class='btn danger text-small'>Delete</button>
                                     </td>
                                     </tr>";
-                                }
                             }
-                            ?>
-                        </table>
-                    </div>
-                    <div class="pagination">
-                        <form id="page-switch" action="" method="get">
-                            <button
-                                class="btn secondary"
-                                type="button"
-                                onclick="handleDecreasePage()"
-                            >
-                                <i class="fa-solid fa-arrow-left"></i>
-                            </button>
-                            <div class="page-input">
-                                <input
-                                    type="number"
-                                    name="page"
-                                    min="1"
-                                    max="<?php echo $totalPages ?>"
-                                    value="<?php echo $currentPage ?>"
-                                /><span>/<?php echo $totalPages ?></span>
-                            </div>
-                            <button
-                                class="btn secondary"
-                                type="button"
-                                onclick="handleIncreasePage()"
-                            >
-                                <i class="fa-solid fa-arrow-right"></i>
-                            </button>
-                        </form>
-                    </div>
+                        }
+                        ?>
+                    </table>
+                </div>
+                <div class="pagination">
+                    <form id="page-switch" action="" method="get">
+                        <button class="btn secondary" type="button" onclick="handleDecreasePage()">
+                            <i class="fa-solid fa-arrow-left"></i>
+                        </button>
+                        <div class="page-input">
+                            <input type="number" name="page" min="1" max="<?php echo $totalPages ?>" value="<?php echo $currentPage ?>" /><span>/<?php echo $totalPages ?></span>
+                        </div>
+                        <button class="btn secondary" type="button" onclick="handleIncreasePage()">
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
-    </body>
-    <script src="./assets/javascript/productListScript.js"></script>
+    </div>
+</body>
+<script src="./assets/javascript/productListScript.js"></script>
+
 </html>
